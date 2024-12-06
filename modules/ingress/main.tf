ac2b40e271bfca42246661ab0dc53892901d1ea5
Rainforest Cheng <contact@rainforest.tools>
2024-12-06 17:32:15 +0800

    feat: Update ingress module to use Helm provider and Traefik chart

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -1,38 +1,15 @@
-# configure the kubernetes provider to use the orbstack cluster
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-  config_context = "orbstack"
-}
-
-resource "kubernetes_pod" "ingress" {
-  metadata {
-    name = "ingress"
-    labels = {
-      app = "ingress"
-    }
-  }
-
-  spec {
-    container {
-      name  = "ingress"
-      image = "nginx:latest"
-    }
+# configure the helm provider to use the orbstack cluster
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    config_context = "orbstack"
   }
 }
 
-# create a service
-resource "kubernetes_service" "ingress" {
-  metadata {
-    name = "ingress"
-  }
-  spec {
-    selector = {
-      app = kubernetes_pod.ingress.metadata.0.labels.app
-    }
-    port {
-      port = 80
-    }
-    type = "NodePort"
-  }
-  depends_on = [ kubernetes_pod.ingress ]
+resource "helm_release" "ingress" {
+  name = "traefik"
+  repository = "https://traefik.github.io/charts"
+  chart = "traefik"
+  create_namespace = true
+  namespace = "homelab"
 }
