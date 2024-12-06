120c362c224bf9cb1bbcf2c229ade426927c6ae0
Rainforest Cheng <contact@rainforest.tools>
2024-12-06 23:01:32 +0800

    feat: Add Flowise module with Helm release configuration

---
+++ modules/flowise/outputs.tf
@@ -0,0 +1,4 @@
+output "flowise_id" {
+  description = "The ID of the flowise resource."
+  value       = helm_release.flowise.id
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "ingress_id" {
   description = "The ID of the ingress resource."
   value       = module.ingress.ingress_id
 }
+
+output "flowise_id" {
+  value = module.flowise.flowise_id
+}
