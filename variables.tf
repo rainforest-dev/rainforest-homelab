86fd0b620acf65211f7449513edd02d196b9f330
Rainforest Cheng <55678466+rainforest-dev@users.noreply.github.com>
2025-09-12 20:09:29 +0800

    Revert "Add Wetty web terminal module with internal-only access"
    This reverts commit 1ba8d33e349c48c1e538f35a7d3087c799550f93.

--- variables.tf
+++ variables.tf
@@ -150,10 +150,7 @@ variable "enable_homepage" {
   default     = true
 }
 
-variable "enable_wetty" {
-  description = "Enable Wetty web terminal (internal access only)"
-  type        = bool
-  default     = false
-}
-
 # Open WebUI Configuration
+#
+#
+#
